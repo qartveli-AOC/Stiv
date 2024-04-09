71360a2c583b1ea273af0028a5548050a0d4dace
qartveli-AOC <samqart@gmail.com>
2024-04-09 16:12:25 +0400

    PP saves Heart

--- Assets/MainGame/Scripts/ReloadGame.cs
+++ Assets/MainGame/Scripts/ReloadGame.cs
@@ -7,19 +7,15 @@ public class ReloadGame : MonoBehaviour
     private Transform cameraMain;
     private Transform handPosition;
     private Animator animator;
-    private Transform player;
+    private Transform player;   
+    
+    
     [SerializeField] SavePoint savePoint;
     
     private void Start()
     {
+        
         savePoint = FindAnyObjectByType<SavePoint>();
-        StaticValue.Heart = PlayerPrefs.GetInt("Heart", 2);
-        StaticValue.thisHeart = PlayerPrefs.GetInt("thisHeart", StaticValue.Heart);
-
-        if (StaticValue.thisHeart <= 0)
-            StaticValue.thisHeart = PlayerPrefs.GetInt("Heart",2);
-
-       
     }
     void OnTriggerEnter(Collider other)
     {
@@ -28,8 +24,6 @@ public class ReloadGame : MonoBehaviour
         handPosition = other.transform.Find("hand");
         player = other.transform.Find("Player 1 Variant");
         animator = cameraMain.GetComponent<Animator>();
-        
-
 
         StartCoroutine(timer());
     }
@@ -38,11 +32,18 @@ public class ReloadGame : MonoBehaviour
         handPosition.gameObject.SetActive(false);
         animator.SetBool("isDeath", true);
         yield return new WaitForSeconds(0.44f);
-        animator.SetBool("isDeath", false);      
-        StaticValue.thisHeart--;  
-        PlayerPrefs.SetInt("thisHeart",StaticValue.thisHeart);
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        animator.SetBool("isDeath", false);
+        DamageHeart();
+        
        
+    }
+    public void DamageHeart()
+    {
+        StaticValue.thisHeart--;
+        if (StaticValue.thisHeart <= 0)
+            savePoint.Reset2();
+        PlayerPrefs.SetInt("thisHeart", StaticValue.thisHeart);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
   
 }
