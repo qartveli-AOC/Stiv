a8d0956e26c1cda784d6e74fdbcee233ecc379ec
dato-svg <139239441+dato-svg@users.noreply.github.com>
2024-04-10 19:42:13 +0400

    save
    save

--- Assets/MainGame/Scripts/UI/RewardManager.cs
+++ Assets/MainGame/Scripts/UI/RewardManager.cs
@@ -1,36 +1,47 @@
+using System.Collections;
 using TMPro;
 using UnityEngine;
 using YG;
 
-namespace UI
+public class RewardManager : MonoBehaviour
 {
-    public class RewardManager : MonoBehaviour
+    [SerializeField] private TMP_Text emeraldTakeText;
+    [SerializeField] private GameObject noRewardButton;
+    [SerializeField] private int GiveEmerald = 10;
+    private int _takeEmerald;
+        
+    private void Start()
     {
-        [SerializeField] private TMP_Text emeraldTakeText;
-        [SerializeField] private int GiveEmerald = 10;
-        private int _takeEmerald;
-        private void Start()
-        {
-            Cursor.visible = true;
-            _takeEmerald = GiveEmerald + StaticValue.EarnPower;
-            emeraldTakeText.text = _takeEmerald.ToString();
-        }
+          
+        _takeEmerald = GiveEmerald + StaticValue.EarnPower;
+        StartCoroutine(ShowRewardButton());
 
-        public void TakeEmerald()
-        {
-            StaticValue.Emerald += _takeEmerald;
-            PlayerPrefs.SetInt("Emerald", StaticValue.Emerald);
-        }
-
-        public void ShowReward()
-        {
-           YandexGame.RewVideoShow(0);
-        }
-
-        public void TakeReward()
-        {
-            StaticValue.Emerald += _takeEmerald * 2;
-            PlayerPrefs.SetInt("Emerald", StaticValue.Emerald);
-        }
     }
+
+    public void TakeEmerald()
+    {
+        StaticValue.Emerald += _takeEmerald;
+        PlayerPrefs.SetInt("Emerald", StaticValue.Emerald);
+    }
+
+    public void ShowReward()
+    {
+        YandexGame.RewVideoShow(0);
+    }
+
+    public void TakeReward()
+    {
+        StaticValue.Emerald += _takeEmerald * 2;
+        PlayerPrefs.SetInt("Emerald", StaticValue.Emerald);
+    }
+
+    public IEnumerator ShowRewardButton()
+    {
+        yield return new WaitForSeconds(1f);
+        noRewardButton.SetActive(true);
+        emeraldTakeText.text = _takeEmerald.ToString();
+    }
+
+      
+        
 }
